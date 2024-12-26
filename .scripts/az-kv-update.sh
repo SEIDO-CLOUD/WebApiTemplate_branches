d8ee7ccd08efc2fd58f637fe9c6e19daea190772
Martin <martin_lenart@icloud.com>
2024-12-26 16:26:19 +0100

    script update

--- .scripts/az-kv-update.sh
+++ .scripts/az-kv-update.sh
@@ -2,14 +2,26 @@
 #To make the .sh file executable
 #sudo chmod +x ./az-kv-update.sh
 
-ApplicationProjectFile="/Users/Martin/Development/goldenProjects/WebApiTemplate_branches/Configuration/Configuration.csproj"
-
+#get the current directory, which is the .scripts directory in the solution directory
 PWDIR=$(pwd)
-cd /Users/Martin/Development/scripts/azure
 
+#set paths and project from the Configuration.csproj file
+ApplicationProjectFile="$PWDIR/../Configuration/Configuration.csproj"
+AzureScriptDirectory=$(sed -n 's:.*<AzureScriptDirectory>\(.*\)</AzureScriptDirectory>.*:\1:p' $ApplicationProjectFile)
 AzureProjectSettings=$(sed -n 's:.*<AzureProjectSettings>\(.*\)</AzureProjectSettings>.*:\1:p' $ApplicationProjectFile)
 
+printf "\n\nSetup parameters\n"
+echo "ApplicationProjectFile="$ApplicationProjectFile
+echo "AzureScriptDirectory="$AzureScriptDirectory
+echo "AzureProjectSettings="$AzureProjectSettings
+
+printf "\n\Updating Keyvault with usersecrets"
+#move into the Azure scripts directory
+cd $AzureScriptDirectory
+
+#execite the scripts to login and update the keyvault with the user secrets
 ./az-login.sh $AzureProjectSettings
 ./az-kv-sec-copy.sh $AzureProjectSettings $ApplicationProjectFile
 
+#come back to this directory
 cd $PWDIR
--- .scripts/prep-publish.sh
+++ .scripts/prep-publish.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 #To make the .sh file executable
 #sudo chmod +x ./publish.sh
-
 if [[ -z "$1" ]]; then
-    printf "\nMissing parameter:\n  ./prep-publish.sh [AppGoodMusicWebApi | AppGoodMusicRazor | AppGoodMusicMVC]\n"
+    printf "\nMissing parameter.\n Parameter should be directory name of application to publish\n"
     exit 1
 fi
-ApplicationToPublish=$1
-#Step1: Set the Azure Keyvault access parameters as operating system environment variables.
-ApplicationProjectFile="/Users/Martin/Development/goldenProjects/GoodMusic_with_lesson_branches/Configuration/Configuration.csproj"
+Application=$1
 
+#get the current directory, which is the .scripts directory in the solution directory
 PWDIR=$(pwd)
-echo $PWDIR
-cd /Users/Martin/Development/scripts/azure
+
+#set paths and project from the Configuration.csproj file
+ApplicationProjectFile="$PWDIR/../Configuration/Configuration.csproj"
+AzureScriptDirectory=$(sed -n 's:.*<AzureScriptDirectory>\(.*\)</AzureScriptDirectory>.*:\1:p' $ApplicationProjectFile)
 AzureProjectSettings=$(sed -n 's:.*<AzureProjectSettings>\(.*\)</AzureProjectSettings>.*:\1:p' $ApplicationProjectFile)
+ApplicationDirectory="$PWDIR/../$Application"
+PublishDirectory="$PWDIR/../$Application/publish"
+
+printf "\n\nSetup parameters\n"
+echo "ApplicationProjectFile="$ApplicationProjectFile
+echo "AzureScriptDirectory="$AzureScriptDirectory
+echo "AzureProjectSettings="$AzureProjectSettings
+echo "ApplicationDirectory="$ApplicationDirectory
+echo "PublishDirectory="$PublishDirectory
+#exit
+
+#Step1: Set the Azure Keyvault access parameters as operating system environment variables.
+cd $AzureScriptDirectory
 
 printf "\n\nSetting the azure key vault access as environent variables"
 export AZURE_TENANT_ID=$(./az-access.sh $AzureProjectSettings tenantId)
@@ -22,6 +35,11 @@ export AZURE_KeyVaultSecret=$(./az-access.sh $AzureProjectSettings kvSecret)
 
 export AZURE_CLIENT_ID=$(./az-access-secrets.sh $AzureProjectSettings app appId)
 export AZURE_CLIENT_SECRET=$(./az-access-secrets.sh $AzureProjectSettings app password)
+
+#Set the environment variables to the ws application
+printf "\n\nSet the environment variables to the azure web application"
+./az-apps-set-env.sh $AzureProjectSettings
+
 cd $PWDIR
 
 #verify environment variables
@@ -34,16 +52,16 @@ echo "AZURE_CLIENT_SECRET=" $AZURE_CLIENT_SECRET
 #Step2: Generate the release files
 printf "\n\nPublish the webapi...\n"
 # #remove any previous publish
-rm -rf ../$ApplicationToPublish/publish
+rm -rf $PublishDirectory
 
-cd ../$ApplicationToPublish
+cd $ApplicationDirectory
 dotnet publish --configuration Release --output ./publish
 
 
 #Step3: Run the application from the folder containing the release files.
 printf "\n\nRun the webapi from the published directory...\n"
-cd ./publish
+cd $PublishDirectory
 
-exec ./$ApplicationToPublish
+exec ./$Application
 
 

3b1f33d815a76b99bb1f6f9b50dc84d7d44551fd
Martin <martin_lenart@icloud.com>
2024-12-30 17:18:39 +0100

    update

--- Configuration/DatabaseConnections.cs
+++ Configuration/DatabaseConnections.cs
@@ -44,7 +44,8 @@ public class DatabaseConnections
             DbConnection = conn.DbConnection,
             DbConnectionString = _configuration.GetConnectionString(conn.DbConnection)
         };
-    }    
+    }
+    
     //Not to revieal the connection string, I find the corresponding DbConnectionKey in the 
     public string GetDbConnection (string DbConnectionString) => 
             _activeDataSet.DbConnections.First(m =>_configuration.GetConnectionString(m.DbConnection).Trim().ToLower() == DbConnectionString.Trim().ToLower())?.DbConnection;

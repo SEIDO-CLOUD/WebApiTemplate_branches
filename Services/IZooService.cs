55844797e9339c373cff812ffd12dd0e11571ce8
Martin <martin_lenart@icloud.com>
2025-01-02 12:58:48 +0100

    update

--- Services/IZooService.cs
+++ Services/IZooService.cs
@@ -8,10 +8,4 @@ public interface IZooService {
     public Task<ResponseItemDto<GstUsrInfoAllDto>> InfoAsync();
     public Task<ResponseItemDto<GstUsrInfoAllDto>> SeedAsync(int nrOfItems);
     public Task<ResponseItemDto<GstUsrInfoAllDto>> RemoveSeedAsync(bool seeded);
-
-    public Task<ResponsePageDto<IZoo>> ReadZoosAsync(bool seeded, bool flat, string filter, int pageNumber, int pageSize);
-    public Task<ResponseItemDto<IZoo>> ReadZooAsync(Guid id, bool flat);
-    public Task<ResponseItemDto<IZoo>> DeleteZooAsync(Guid id);
-    public Task<ResponseItemDto<IZoo>> UpdateZooAsync(ZooCuDto item);
-    public Task<ResponseItemDto<IZoo>> CreateZooAsync(ZooCuDto item);
 }
--- Services/ZooServiceDb.cs
+++ Services/ZooServiceDb.cs
@@ -9,27 +9,16 @@ namespace Services;
 public class ZooServiceDb : IZooService {
 
     private readonly AdminDbRepos _adminRepo;
-    private readonly ZooDbRepos _zooRepo;
     private readonly ILogger<ZooServiceDb> _logger;    
     
-    public ZooServiceDb(AdminDbRepos adminRepo, ZooDbRepos zooRepo, ILogger<ZooServiceDb> logger)
+    public ZooServiceDb(AdminDbRepos adminRepo, ILogger<ZooServiceDb> logger)
     {
         _adminRepo = adminRepo;
-        _zooRepo = zooRepo;
         _logger = logger;
     }
 
-    #region Simple 1:1 calls in this case, but as Services expands, this will no longer be the case
     public Task<ResponseItemDto<GstUsrInfoAllDto>> InfoAsync() => _adminRepo.InfoAsync();
-
     public Task<ResponseItemDto<GstUsrInfoAllDto>> SeedAsync(int nrOfItems) => _adminRepo.SeedAsync(nrOfItems);
     public Task<ResponseItemDto<GstUsrInfoAllDto>> RemoveSeedAsync(bool seeded) => _adminRepo.RemoveSeedAsync(seeded);
 
-    public Task<ResponsePageDto<IZoo>> ReadZoosAsync(bool seeded, bool flat, string filter, int pageNumber, int pageSize) => _zooRepo.ReadItemsAsync(seeded, flat, filter, pageNumber, pageSize);
-    public Task<ResponseItemDto<IZoo>> ReadZooAsync(Guid id, bool flat) => _zooRepo.ReadItemAsync(id, flat);
-    public Task<ResponseItemDto<IZoo>> DeleteZooAsync(Guid id) => _zooRepo.DeleteItemAsync(id);
-    public Task<ResponseItemDto<IZoo>> UpdateZooAsync(ZooCuDto item) => _zooRepo.UpdateItemAsync(item);
-    public Task<ResponseItemDto<IZoo>> CreateZooAsync(ZooCuDto item) => _zooRepo.CreateItemAsync(item);
-
-    #endregion
 }

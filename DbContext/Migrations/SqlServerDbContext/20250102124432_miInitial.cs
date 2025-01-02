8c9c46e987407c2c6f01c64e2ea1cf294772f738
Martin <martin_lenart@icloud.com>
2025-01-02 13:45:00 +0100

    update

---
+++ DbContext/Migrations/SqlServerDbContext/20250102124432_miInitial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContext.Migrations.SqlServerDbContext
+{
+    /// <inheritdoc />
+    public partial class miInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Zoos",
+                columns: table => new
+                {
+                    ZooId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zoos", x => x.ZooId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Zoos");
+        }
+    }
+}

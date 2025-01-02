4fe58b32d4a270ba20734840a462f875589c3c0f
Martin <martin_lenart@icloud.com>
2025-01-02 11:50:20 +0100

    update

--- DbRepos/EmployeeRepos.cs
+++ DbRepos/EmployeeRepos.cs
@@ -27,16 +27,12 @@ public class EmployeeDbRepos
         IQueryable<EmployeeDbM> query;
         if (!flat)
         {
-            //make sure the model is fully populated, try without include.
-            //remove tracking for all read operations for performance and to avoid recursion/circular access
             query = _dbContext.Employees.AsNoTracking()
                 .Include(i => i.ZoosDbM)
                 .Where(i => i.EmployeeId == id);
         }
         else
         {
-            //Not fully populated, compare the SQL Statements generated
-            //remove tracking for all read operations for performance and to avoid recursion/circular access
             query = _dbContext.Employees.AsNoTracking()
                 .Where(i => i.EmployeeId == id);
         }

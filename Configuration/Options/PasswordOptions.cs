e86a84a2c6cdb8e31ee906e233d0c7d899031b55
Martin <martin_lenart@icloud.com>
2025-01-03 18:04:00 +0100

    update

--- Configuration/Options/PasswordOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Configuration;
-
-public class PasswordOptions
-{
-    public const string Position = "PasswordSaltDetails";
-    public string Salt { get; set; }
-    public int Iterations { get; set; }
-}

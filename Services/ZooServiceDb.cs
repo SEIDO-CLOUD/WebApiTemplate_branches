5c077faff7452f534643498c889ad54d4c7460bc
Martin <martin_lenart@icloud.com>
2025-01-04 12:52:08 +0100

    adminService

--- Services/ZooServiceDb.cs
+++ Services/ZooServiceDb.cs
@@ -8,20 +8,14 @@ namespace Services;
 
 public class ZooServiceDb : IZooService {
 
-    private readonly AdminDbRepos _adminRepo;
     private readonly ZooDbRepos _zooRepo;
     private readonly ILogger<ZooServiceDb> _logger;    
     
-    public ZooServiceDb(AdminDbRepos adminRepo, ZooDbRepos zooRepo, ILogger<ZooServiceDb> logger)
+    public ZooServiceDb(ZooDbRepos zooRepo, ILogger<ZooServiceDb> logger)
     {
-        _adminRepo = adminRepo;
         _zooRepo = zooRepo;
         _logger = logger;
     }
-
-    public Task<ResponseItemDto<GstUsrInfoAllDto>> InfoAsync() => _adminRepo.InfoAsync();
-    public Task<ResponseItemDto<GstUsrInfoAllDto>> SeedAsync(int nrOfItems) => _adminRepo.SeedAsync(nrOfItems);
-    public Task<ResponseItemDto<GstUsrInfoAllDto>> RemoveSeedAsync(bool seeded) => _adminRepo.RemoveSeedAsync(seeded);
 
     public Task<ResponsePageDto<IZoo>> ReadZoosAsync(bool seeded, bool flat, string filter, int pageNumber, int pageSize) => _zooRepo.ReadItemsAsync(seeded, flat, filter, pageNumber, pageSize);
     public Task<ResponseItemDto<IZoo>> ReadZooAsync(Guid id, bool flat) => _zooRepo.ReadItemAsync(id, flat);

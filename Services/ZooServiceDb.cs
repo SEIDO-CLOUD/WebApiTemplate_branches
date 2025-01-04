3f0840496e9f809343fdb49df864bc881838fef7
Martin <martin_lenart@icloud.com>
2025-01-04 12:05:19 +0100

    update

--- Services/ZooServiceDb.cs
+++ Services/ZooServiceDb.cs
@@ -27,7 +27,6 @@ public class ZooServiceDb : IZooService {
     }
 
     public Task<ResponseItemDto<GstUsrInfoAllDto>> InfoAsync() => _adminRepo.InfoAsync();
-
     public Task<ResponseItemDto<GstUsrInfoAllDto>> SeedAsync(int nrOfItems) => _adminRepo.SeedAsync(nrOfItems);
     public Task<ResponseItemDto<GstUsrInfoAllDto>> RemoveSeedAsync(bool seeded) => _adminRepo.RemoveSeedAsync(seeded);
 

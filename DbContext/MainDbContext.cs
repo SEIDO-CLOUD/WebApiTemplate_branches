18ecbbdecdc7b4fb02799baa6e4e046c5d806834
Martin <martin_lenart@icloud.com>
2025-01-05 14:27:39 +0100

    update

--- DbContext/MainDbContext.cs
+++ DbContext/MainDbContext.cs
@@ -24,7 +24,7 @@ public class MainDbContext : Microsoft.EntityFrameworkCore.DbContext
     public string dbConnection  => _databaseConnections.GetDbConnection(this.Database.GetConnectionString());
 
     #region C# model of database tables
-    public DbSet<TemplateModelDbM> Zoos { get; set; }
+    public DbSet<TemplateModelDbM> TemplateModel { get; set; }
     #endregion
 
     #region model the Views

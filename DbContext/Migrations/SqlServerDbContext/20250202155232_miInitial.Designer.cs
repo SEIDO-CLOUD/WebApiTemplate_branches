cce8dfc3dbafaadcdcfa311a22ec2cc7f49b3825
Martin <martin_lenart@icloud.com>
2025-02-03 08:14:49 +0100

    update

--- DbContext/Migrations/SqlServerDbContext/20250202155232_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20250202155232_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20250131111139_miInitial")]
+    [Migration("20250202155232_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />

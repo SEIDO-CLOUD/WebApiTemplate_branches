22b75a942dd103947f8299eec57c03170c108909
Martin <martin_lenart@icloud.com>
2025-02-11 18:05:41 +0100

    update

--- AppWebApiTest/Worker.cs
+++ AppWebApiTest/Worker.cs
@@ -16,7 +16,7 @@ public class Worker : BackgroundService
         _logger.LogInformation("Hello World again");
         //throw new Exception("Error in AppSimple");
 // Ensure the application exits with result code 1
-            Environment.Exit(1);
         await _host.StopAsync();
+            Environment.Exit(1);
     }
 }
